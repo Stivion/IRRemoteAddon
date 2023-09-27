b8e93faf8ee7a908c6902635c42f3fceaabee512
Andrey Alimaev <stivion@gmail.com>
2023-09-27 21:57:40 +0300

    Install and start addon on publish. Install python3 in Docker container.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ ARG BUILD_FROM
 
 FROM $BUILD_FROM
 
+RUN apk add --no-cache python3
+
 COPY run.sh /
 COPY src/* /
 RUN chmod a+x /run.sh
--- build/deploy.sh
+++ build/deploy.sh
@@ -5,4 +5,22 @@ NoColor='\033[0m'
 
 archiveName=$1
 
+# Stop on error
+set -e
+
 rm $archiveName.zip
+
+RebuildAddon() {
+    printf "${Green}Rebuilding addon${NoColor}\n"
+    ha addons rebuild "local_${archiveName}" || (printf "${Green}Addon is not installed. Installing${NoColor}\n" && ha addons install "local_${archiveName}")
+}
+
+StartAddon() {
+    printf "${Green}Starting addon${NoColor}\n"
+    ha addons start "local_${archiveName}"
+}
+
+RebuildAddon
+StartAddon
+
+printf "${Green}Deploy completed successfully${NoColor}\n"
