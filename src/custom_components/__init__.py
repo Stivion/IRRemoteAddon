417ff2e35898c40f53c5ac4e1e7613992b480ae7
Andrey Alimaev <stivion@gmail.com>
2023-11-06 14:39:23 +0300

    Add AC controls

--- src/custom_components/__init__.py
+++ src/custom_components/__init__.py
@@ -4,13 +4,21 @@ from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
 import time
 
-HANDLER_ADDRESS = 'http://192.168.50.91'
 DOMAIN = "ir_remote_control"
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
+    host = config[DOMAIN].get('host', None)
+    if host is None:
+        return False
+    
+    if host.endswith('/'):
+        host = host[:-1]
+
     session = async_create_clientsession(hass)
+    ac_state = await (await session.get(f'{host}/ac/state')).json()
+    hass_set_ac_state(hass, ac_state)
 
     # Soundbar controls
     @callback
@@ -19,7 +27,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
         amount = int(data.pop('amount', 1))
         for _ in range(amount):
-            await session.post(f'{HANDLER_ADDRESS}/soundbar/volume-up', proxy=None, ssl=False)
+            await session.post(f'{host}/soundbar/volume-up', proxy=None, ssl=False)
             time.sleep(0.5)
 
 
@@ -29,31 +37,62 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
         amount = int(data.pop('amount', 1))
         for _ in range(amount):
-            await session.post(f'{HANDLER_ADDRESS}/soundbar/volume-down', proxy=None, ssl=False)
+            await session.post(f'{host}/soundbar/volume-down', proxy=None, ssl=False)
             time.sleep(0.5)
 
 
     @callback
     async def toggle_input(call: ServiceCall) -> None:
-        await session.post(f'{HANDLER_ADDRESS}/soundbar/toggle-input', proxy=None, ssl=False)
+        await session.post(f'{host}/soundbar/toggle-input', proxy=None, ssl=False)
 
 
     # AC controls
     @callback
     async def power_on(call: ServiceCall) -> None:
-        pass
+        if ac_state['power'] == True:
+            return
+        
+        ac_state['power'] = True
+        hass_set_ac_state(hass, ac_state)
+        await session.post(f'{host}/ac/state', json=ac_state, proxy=None, ssl=False)
+
 
     @callback
     async def power_off(call: ServiceCall) -> None:
-        pass
+        if ac_state['power'] == False:
+            return
+        
+        ac_state['power'] = False
+        hass_set_ac_state(hass, ac_state)
+        await session.post(f'{host}/ac/state', json=ac_state, proxy=None, ssl=False)
+
 
     @callback
     async def set_mode(call: ServiceCall) -> None:
-        pass
+        data = dict(call.data)
+        mode = int(data.pop('mode', 0))
+        if mode < 0 or mode > 4:
+            return
+        if ac_state['mode'] == mode:
+            return
+        
+        ac_state['mode'] = mode
+        hass_set_ac_state(hass, ac_state)
+        await session.post(f'{host}/ac/state', json=ac_state, proxy=None, ssl=False)
+
 
     @callback
     async def set_temp(call: ServiceCall) -> None:
-        pass
+        data = dict(call.data)
+        tempC = int(data.pop('tempC', 24))
+        if tempC < 16 or tempC > 30:
+            return
+        if ac_state['tempC'] == tempC:
+            return
+        
+        ac_state['tempC'] = tempC
+        hass_set_ac_state(hass, ac_state)
+        await session.post(f'{host}/ac/state', json=ac_state, proxy=None, ssl=False)
 
     
     hass.services.async_register(DOMAIN, 'volume_up', volume_up)
@@ -66,3 +105,8 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     hass.services.async_register(DOMAIN, 'set_temp', set_temp)
 
     return True
+
+
+def hass_set_ac_state(hass, ac_state):
+    powerOn = ac_state['power']
+    hass.states.async_set("ir_remote_control.ac", 'on' if powerOn else 'off', ac_state)
